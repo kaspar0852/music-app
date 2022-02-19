ecb80967f8e1011de1d8849d4f12cf1f2acda129
iamshawnsree <yathartha.subedi@gmail.com>
2022-02-19 19:35:53 +0545

    feat: login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:musicplayer/screens/login.dart';
 import 'package:musicplayer/screens/nowplaing.dart';
 import 'package:musicplayer/screens/library.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Library(),
+    home: MyStatefulWidget(),
   ));
 }
